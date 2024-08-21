78c4ea7e1a8e95449c45f10e60f28d64faf2b7ff
Darr Or <darrmorgan@gmail.com>
2024-08-21 18:47:33 -0400

    Add img service

--- src/RootCmp.jsx
+++ src/RootCmp.jsx
@@ -5,12 +5,18 @@ import { store } from './store/store.js'
 import { StationIndex } from './pages/StationIndex.jsx'
 import { MainView } from './pages/MainView.jsx'
 import { Search } from './pages/Search.jsx'
+import { imgService } from './services/imgService.js'
 
 import './assets/style/main.scss'
 
 export function App() {
     return (
+
+        // <AppHeader />
+        // <UserMsg />
+        
         <Provider store={store}>
+            <img src={imgService.getImg('liked')} alt="" />
             <Router>
                 <Routes>
                     <Route path="/" element={<StationIndex />}>
