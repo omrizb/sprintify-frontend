2736ca7407ce9f4d91afbfacfd9c928ef0d1aa6d
Darr Or <darrmorgan@gmail.com>
2024-08-21 22:18:03 -0400

    Add svg service and component for icons

--- src/RootCmp.jsx
+++ src/RootCmp.jsx
@@ -6,6 +6,7 @@ import { StationIndex } from './pages/StationIndex.jsx'
 import { MainView } from './pages/MainView.jsx'
 import { Search } from './pages/Search.jsx'
 import { imgService } from './services/imgService.js'
+import SvgIcon from './cmps/SvgIcon.jsx'
 
 import './assets/style/main.scss'
 
@@ -17,6 +18,7 @@ export function App() {
         
         <Provider store={store}>
             <img src={imgService.getImg('liked')} alt="" />
+            <SvgIcon iconName={"bell"} />
             <Router>
                 <Routes>
                     <Route path="/" element={<StationIndex />}>
---
+++ src/cmps/SvgIcon.jsx
@@ -0,0 +1,8 @@
+import { svgService } from "../services/svg.service"
+
+const svgIcon = ({ iconName }) => {
+  const svg = svgService.getSvg(iconName);
+  return <i dangerouslySetInnerHTML={{ __html: svg }}></i>
+}
+
+export default svgIcon
