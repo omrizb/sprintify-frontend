06c24bf14cbbfeebc9a2fbd961ce71db3085c9d4
Darr Or <darrmorgan@gmail.com>
2024-08-23 00:14:56 -0400

    Create initial station preview and connect to station list

--- src/cmps/StationList.jsx
+++ src/cmps/StationList.jsx
@@ -1,11 +1,17 @@
+import { StationPreview } from "./StationPreview.jsx"
 
 
-export function StationList() {
+export function StationList({stations}) {
 
-    // const stations = useSelector(storeState => storeState.stationModule.stations)
     return (
-        <div className="station-list">
-            Station List
-        </div>
+        <section>
+        <ul className="station-list">
+            {stations.map(station =>
+                <li key={station._id}>
+                    <StationPreview station={station}/>
+                </li>)
+            }
+        </ul>
+    </section>
     )
 }
---
+++ src/cmps/StationPreview.jsx
@@ -0,0 +1,7 @@
+
+export function StationPreview({ station }) {
+    return <article className="preview">
+        {station.name}
+        
+    </article>
+}
