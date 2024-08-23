4de69679ad9d40cd7e986c2fe8d7b8003fb5f72d
Darr Or <darrmorgan@gmail.com>
2024-08-23 00:02:56 -0400

    Cיhange stations actions to connect to local service

--- src/store/actions/station.actions.js
+++ src/store/actions/station.actions.js
@@ -1,4 +1,4 @@
-import { stationService } from '../../services/station'
+import { stationService } from '../../services/station/station.service.local.js'
 import { store } from '../store.js'
 import { ADD_STATION, REMOVE_STATION, SET_STATIONS, SET_STATION, UPDATE_STATION, ADD_STATION_MSG } from '../reducers/station.reducer.js'
 
