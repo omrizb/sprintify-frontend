90900cd891b01df834357e546a42062df9d7f372
einat berger <einatguberman@gmail.com>
2024-08-25 17:20:53 +0200

    Fix and add classNames add svg and change prop to come from details

--- src/cmps/StationDetails/StationDetailsHeader.jsx
+++ src/cmps/StationDetails/StationDetailsHeader.jsx
@@ -1,23 +1,32 @@
 
-export function StationDetailsHeader({ stationMeta, songCountTxt, durationTxt }) {
-    const { type, isLikedSongs, isEmptyStation, isOwnedByUser } = stationMeta
+import { SvgIcon } from "../SvgIcon"
 
-    const showSongCountTxt = type === 'playlist' && !isEmptyStation
-    const showDurationTxt = type === 'playlist' && !isLikedSongs
+export function StationDetailsHeader({ station, showSongCountTxt, songCountTxt, showDurationTxt, durationTxt }) {
 
     return (
         <div className="station-header">
-            <img className="station-cover" src={station.imgUrl} alt="Station Cover" />
-            <div className="station-info">
-                <p>{station.type}</p>
-                <h1>{station.name}</h1>
-                <p>
-                    {station.createdBy.fullName}
-                    {showSongCountTxt && <><span className="dot">{songCountTxt()}</span></>}
-                    {showDurationTxt && <><span>, </span><span>{durationTxt()}</span></>}
-                </p>
+            <div className="station-cover-container">
+                <img className="station-cover" src={station.imgUrl} alt="Station Cover" />
+                <div className="overlay">
+                    <div className="overlay-content">
+                        <button className="editImage icon btn-medium">
+                            <SvgIcon iconName={"editImage"} />
+                        </button>
+                        <span>Choose photo</span>
+                    </div>
+                </div>
             </div>
-        </div>
+
+            <div className="station-info">
+                <div className="station-type">{station.type}</div>
+                <div className="station-name">{station.name}</div>
+                <div>
+                    <span className="station-createdBy">{station.createdBy.fullName}</span>
+                    {showSongCountTxt && <><span className="dot">{songCountTxt}</span></>}
+                    {showDurationTxt && <><span>, </span><span>{durationTxt}</span></>}
+                </div>
+            </div>
+        </div >
     )
 }
 
