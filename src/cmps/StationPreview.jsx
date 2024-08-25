61ea98b8791be646b249625bca41550ed0bc1d52
Darr Or <darrmorgan@gmail.com>
2024-08-25 00:50:36 -0400

    Add img and description to station preview

--- src/cmps/StationPreview.jsx
+++ src/cmps/StationPreview.jsx
@@ -1,8 +1,12 @@
 
 export function StationPreview({ station }) {
-    return <article className="preview">
-        <img src="station.stationImgUrl" alt="" />
-        {station.name}
+
+    return <article className="station-preview">
+        <img src={station.stationImgUrl} alt="" />
+        <section className="text">
+            <div className="station-name">{station.name}</div>
+            <div>{station.type} • {station.createdBy.fullName}</div>
+        </section>
         
     </article>
 }
