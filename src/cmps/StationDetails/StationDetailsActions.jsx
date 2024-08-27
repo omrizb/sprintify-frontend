97d035aa302a16f7b7c91b9c420f9db88c538f47
einat berger <einatguberman@gmail.com>
2024-08-27 21:33:19 +0200

    small css change in StationDetailsActions

--- src/cmps/StationDetails/StationDetailsActions.jsx
+++ src/cmps/StationDetails/StationDetailsActions.jsx
@@ -37,59 +37,60 @@ export function StationDetailsActions({ stationMeta }) {
     return (
         <div className="station-action-bar">
             <div className="station-action-bar-row">
-                {showPlay && (
-                    <div className="wrap-playPlaylist">
-                        <button className="action-bar-btn playPlaylist icon btn-medium">
-                            <SvgIcon iconName={"playPlaylist"} />
-                        </button>
-                    </div>
-                )}
+                <div className="station-action-bar-container">
+                    {showPlay && (
+                        <div className="wrap-playPlaylist">
+                            <button className="action-bar-btn playPlaylist icon btn-medium">
+                                <SvgIcon iconName={"playPlaylist"} />
+                            </button>
+                        </div>
+                    )}
 
-                {showAddRemove && (
-                    <>
-                        <button className="action-bar-btn removePlaylist icon btn-medium">
-                            <SvgIcon iconName={"removePlaylist"} />
-                        </button>
-                        <button className="action-bar-btn addPlaylist icon btn-medium">
-                            <SvgIcon iconName={"addPlaylist"} />
-                        </button>
-                    </>
-                )}
+                    {showAddRemove && (
+                        <>
+                            <button className="action-bar-btn removePlaylist icon btn-medium">
+                                <SvgIcon iconName={"removePlaylist"} />
+                            </button>
+                            <button className="action-bar-btn addPlaylist icon btn-medium">
+                                <SvgIcon iconName={"addPlaylist"} />
+                            </button>
+                        </>
+                    )}
 
-                {showFollowUnfollow && (
-                    <>
-                        <button className="following-btn">Following</button>
-                        <button className="following-btn">Follow</button>
-                    </>
-                )}
+                    {showFollowUnfollow && (
+                        <>
+                            <button className="following-btn">Following</button>
+                            <button className="following-btn">Follow</button>
+                        </>
+                    )}
 
-                {showMore && (
-                    <button className="action-bar-btn dots icon btn-medium">
-                        <SvgIcon iconName={"dots"} />
-                    </button>
-                )}
-
-                {showViewList && (
-                    <div className="view-mode-btn">
-                        <button className="action-bar-btn list icon btn-medium" onClick={() => handleViewModeClick('list')}>
-                            <span>List</span>
-                            <SvgIcon iconName={"list"} />
+                    {showMore && (
+                        <button className="action-bar-btn dots icon btn-medium">
+                            <SvgIcon iconName={"dots"} />
                         </button>
-                    </div>
-                )}
-                {showViewCompact && (
-                    <div className="view-mode-btn">
-                        <button className="action-bar-btn compact icon btn-medium" onClick={() => handleViewModeClick('compact')}>
-                            <span>Compact</span>
-                            <SvgIcon iconName={"compact"} />
-                        </button>
-                    </div>
-                )}
+                    )}
 
+                    {showViewList && (
+                        <div className="view-mode-btn">
+                            <button className="action-bar-btn list icon btn-medium" onClick={() => handleViewModeClick('list')}>
+                                <span>List</span>
+                                <SvgIcon iconName={"list"} />
+                            </button>
+                        </div>
+                    )}
+                    {showViewCompact && (
+                        <div className="view-mode-btn">
+                            <button className="action-bar-btn compact icon btn-medium" onClick={() => handleViewModeClick('compact')}>
+                                <span>Compact</span>
+                                <SvgIcon iconName={"compact"} />
+                            </button>
+                        </div>
+                    )}
+                </div>
                 <div className="view-as">
                     <div onClick={() => setShowMenu(prevShowMenu => !prevShowMenu)} className="recents" >
                         List
-                        <div className="icon"><SvgIcon iconName={"list"}    /> </div>
+                        <div className="icon"><SvgIcon iconName={"list"} /> </div>
                     </div>
                     {showMenu && <DropDownMenu display={display} />}
                 </div>
