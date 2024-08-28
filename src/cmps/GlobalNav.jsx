a814ba73263623cb9fede4c9c948672cad024e61
Darr Or <darrmorgan@gmail.com>
2024-08-27 23:06:46 -0400

    Connect search value in global header to youtube service to fetch songs

--- src/cmps/GlobalNav.jsx
+++ src/cmps/GlobalNav.jsx
@@ -1,13 +1,35 @@
 import { Link } from 'react-router-dom'
+
 import { SvgButton } from "./SvgButton.jsx"
 import { SvgIcon } from './SvgIcon'
+import { youtubeService } from '../services/youtube.service.js'
+import { utilService } from '../services/util.service.js'
 
 
 export function GlobalNav() {
+
+    const debouncedLoadSongs = utilService.debounce(loadSongs, 1000) 
+
+    async function loadSongs(value) {
+        try {
+            const songs = await youtubeService.getVideos(value)
+            console.log(songs)
+
+        } catch (err) {
+            console.log(`Couldn't load videos`, err)
+        }
+    }
+
+    function handleChange(ev) {
+        var value = ev.target.value
+        debouncedLoadSongs(value)
+        console.log(value)
+    }
+
+
     return (
         <div className="global-nav">
-            {/* <img src={imgService.getImg('liked')} alt="" /> */}
-
+            
             <div className="spotify"><SvgIcon iconName={"spotify"} /></div>
             <Link to={`/`}>
                 <SvgButton btnClass={'btn-tinted home'} svgIcon={'home'} />  
@@ -16,15 +38,19 @@ export function GlobalNav() {
             <Link to={`/search`}>
                 <div className="text-container">
                     <div className="search icon"><SvgIcon iconName={"search"}    /> </div>
-                    <input type="text" placeholder="What do you want to play?" /> 
+                    <input 
+                            type="text" 
+                            name="txt"
+                            
+                            placeholder="What do you want to play?"
+                            onChange={handleChange}
+                            required
+                        />
                     <div className="browse icon"><SvgIcon iconName={"browse"}    /> </div> 
                 </div>
             </Link>
             <div className="bell icon"><SvgIcon iconName={"bell"} /></div>
             <button className="profile-btn">D</button>
-
-
-
 
         </div>
     )
