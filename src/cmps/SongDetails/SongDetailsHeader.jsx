c6d81becb7b395cb8f45578cfa5a2ad1c543843c
einat berger <einatguberman@gmail.com>
2024-08-28 14:17:41 +0200

    create SongDetailsHeader

---
+++ src/cmps/SongDetails/SongDetailsHeader.jsx
@@ -0,0 +1,30 @@
+
+import { SvgIcon } from "../SvgIcon"
+
+export function SongDetailsHeader({ song, songDurationTxt, viewMode }) {
+
+    return (
+        <div className="song-details-header">
+            <div className="song-cover-container">
+                <img className="song-cover" src={song.imgUrl} alt={song.songName} />
+            </div>
+
+            <div className="song-info">
+                <div className="type">Song</div>
+                <div className="song-name">{song.songName}</div>
+                <div>
+                    <span>{song.artist}</span>
+                    •
+                    <span>{song.album}</span>
+                    •
+                    <span>{new Date(song.publishedAt).getFullYear()}</span>
+                    •
+                    <span>{songDurationTxt}</span>
+                    •
+                    <span>{song.playCount}</span>
+                </div>
+            </div>
+        </div >
+    )
+}
+
