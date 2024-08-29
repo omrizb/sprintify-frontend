e583266d25109ccb96b86e3171f0d2b7b44be46e
Darr Or <darrmorgan@gmail.com>
2024-08-29 02:18:20 -0400

    Move the duration calculation to header to make station details more readable

--- src/cmps/StationDetails/StationDetailsHeader.jsx
+++ src/cmps/StationDetails/StationDetailsHeader.jsx
@@ -1,11 +1,28 @@
-
 import { SvgIcon } from "../SvgIcon"
 
-export function StationDetailsHeader({ station, songCountTxt, durationTxt  }) {
+export function StationDetailsHeader({ station  }) {
 
+    const songCountTxt = formatSongCountTxt()
+    const durationTxt = formatDurationTxt()
+
+    function formatSongCountTxt() {
+        const songCount = station.songs.length
+        return songCount > 1 ? ` • ${songCount} songs` : ` • ${songCount} song`
+    }
+
+    function formatDurationTxt() {
+        const totalDuration = station.songs.reduce((acc, song) => {
+            return acc + song.duration.hours * 3600 + song.duration.minutes * 60 + song.duration.seconds
+        }, 0)
+
+        const hours = Math.floor(totalDuration / 3600)
+        const minutes = Math.floor((totalDuration % 3600) / 60)
+        const seconds = totalDuration % 60
+
+        return `,${hours > 0 ? `${hours} hr ` : ''}${minutes > 0 ? `${minutes} min ` : ''}${seconds > 0 ? `${seconds} sec` : ''}`
+    }
 
     
- 
     return (
         <div className="station-details-header">
             <div className="station-cover-container">
