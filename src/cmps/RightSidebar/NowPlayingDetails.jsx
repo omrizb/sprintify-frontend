17af38813ca98afb4ac83ac38952edfa6027cbc0
einat berger <einatguberman@gmail.com>
2024-08-30 17:13:15 +0200

    Add using SongPreview to show the songs at NowPlayingDetails

--- src/cmps/RightSidebar/NowPlayingDetails.jsx
+++ src/cmps/RightSidebar/NowPlayingDetails.jsx
@@ -1,12 +1,16 @@
 import { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom';
 
 import { youtubeService } from '../../services/youtube.service'
+import { SongPreview } from '../SongDetails/SongPreview.jsx'
+import { SvgButton } from '../SvgButton.jsx'
 
 export function NowPlayingDetails() {
 
     const player = useSelector(state => state.playerModule.player)
     const stationName = useSelector(state => state.playerModule.stationName)
+    const [isLoading, setIsLoading] = useState(true)
     const [song, setSong] = useState(null)
 
     useEffect(() => {
@@ -14,15 +18,52 @@ export function NowPlayingDetails() {
     }, [player.songId])
 
     async function loadSong(songId) {
-        const ytSong = await youtubeService.getSongById(songId)
-        setSong(ytSong)
+        setIsLoading(true)
+        try {
+            const ytSong = await youtubeService.getSongById(songId)
+            setSong(ytSong)
+        } catch (err) {
+            console.error(`Couldn't load song from song details`, err)
+        } finally {
+            setIsLoading(false)
+        }
     }
 
     return (
         <div className="now-playing-details">
-            <h2>Station: {stationName}</h2>
-            <pre>{JSON.stringify(song, false, 4)}</pre>
+            {isLoading && <div>Loading...</div>}
+            {song && (
+                <div className='now-playing-details-header'>
+                    <div className='header-container'>
+                        <Link to={`/track/${song.songId}`}>
+                            <div className='link'>{song.songName}</div>
+                        </Link>
+                        <div className='action-bar-row'>
+                            <div>
+                                <SvgButton
+                                    btnClass={"btn-action-bar"}
+                                    svgIcon={"dots"}
+                                    svgClass={"svg-small"}
+                                    tooltipTxt={`More options for ${song.songName}`}
+                                />
+                                <SvgButton
+                                    btnClass={"btn-action-bar"}
+                                    svgIcon={"close"}
+                                    svgClass={"svg-small"}
+                                    tooltipTxt={"close"}
+                                />
+                            </div>
+                        </div>
+                    </div>
+                    {/* <pre>{JSON.stringify(song, false, 4)}</pre> */}
+                    <SongPreview song={song} index={0} style={'card'} />
+                    <SongPreview song={song} index={0} style={'card'} />
+                    <SongPreview song={song} index={0} style={'card'} />
+                    <SongPreview song={song} index={0} style={'card'} />
 
+                </div>
+            )}
         </div>
     )
+
 }
