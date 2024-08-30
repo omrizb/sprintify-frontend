a7a321d33f0505c71c00bc1bc59a284d2605ce52
einat berger <einatguberman@gmail.com>
2024-08-30 17:18:51 +0200

    Add using SongPreview to show songs in SearchResults

--- src/pages/SearchResults.jsx
+++ src/pages/SearchResults.jsx
@@ -2,9 +2,10 @@ import { useParams } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 import { youtubeService } from '../services/youtube.service.js'
 import { utilService } from '../services/util.service.js'
+import { SongPreview } from '../cmps/SongDetails/SongPreview.jsx'
 
 export function SearchResults() {
-
+    const [isLoading, setIsLoading] = useState(true)
     const { txt } = useParams()
     const [songs, setSongs] = useState([])
 
@@ -13,12 +14,14 @@ export function SearchResults() {
     }, [])
 
     async function loadSongs(value) {
+        setIsLoading(true)
         try {
             const loadedSongs = await youtubeService.getVideos(value)
             setSongs(loadedSongs)
-
         } catch (err) {
-            console.log(`Couldn't load videos`, err)
+            console.error(`Couldn't load videos`, err)
+        } finally {
+            setIsLoading(false)
         }
     }
 
@@ -27,11 +30,13 @@ export function SearchResults() {
         <div>
             <h1>Search Results:</h1>
 
-            {(!songs) && <h2> Youtube is blocking us!!</h2> }
+            {(!songs) && <h2> Youtube is blocking us!!</h2>}
             {songs &&
                 <ul >
-                    {songs.map(song =>
-                        <li key={song.songId} >{song.songName}</li>)
+                    {songs.map((song, index) =>
+                        <li key={song.songId} >
+                            <SongPreview song={song} index={0} style={'search'} />
+                        </li>)
                     }
                 </ul>}
         </div>
