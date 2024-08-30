24f43a58be8768701684743c526168a163029f8f
einat berger <einatguberman@gmail.com>
2024-08-30 17:25:56 +0200

    Adding AddSong for recommended at songDetails page

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -1,75 +1,95 @@
 import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
-import { SongPreview } from '../cmps/StationDetails/SongPreview'
+
 import { youtubeService } from '../services/youtube.service.js'
-import { Footer } from '../cmps/Footer.jsx'
 import { utilService } from '../services/util.service.js'
 import { SongDetailsHeader } from '../cmps/SongDetails/SongDetailsHeader.jsx'
 import { SongDetailsActions } from '../cmps/SongDetails/SongDetailsActions.jsx'
 import { LongTxt } from '../cmps/LongTxt.jsx'
+import { AddSongs } from '../cmps/AddSongs.jsx'
+import { Footer } from '../cmps/Footer.jsx'
 
 
 export function SongDetails() {
 
     const { id } = useParams()
-    const [songYT, setSongYT] = useState({})
+    const [isLoading, setIsLoading] = useState(true)
+    const [song, setSong] = useState(null)
     const [bgColor, setBgColor] = useState(utilService.getRandomColor())
 
 
     useEffect(() => {
         setBgColor(utilService.getRandomColor())
         loadSong(id)
-    }, [])
+    }, [id])
 
-    async function loadSong(songId) {
+    async function loadSong(id) {
+        setIsLoading(true)
         try {
-            const song = await youtubeService.getSongById(songId)
-            setSongYT(song)
+            const song = await youtubeService.getSongById(id)
+            setSong(song)
         } catch (err) {
             console.error(`Couldn't load song from song details`, err)
+        } finally {
+            setIsLoading(false)
         }
     }
 
-    const songDuration =
-        songYT.duration
-            ? (songYT.duration.hours * 3600) + (songYT.duration.minutes * 60) + songYT.duration.seconds
-            : 0;
+    const songDuration = !song
+        ? 0
+        : (song.duration.hours * 3600) + (song.duration.minutes * 60) + song.duration.seconds
 
-    const songDurationTxt = utilService.getTimeStr(songDuration);
+    const songDurationTxt = utilService.getTimeStr(songDuration)
 
     const lyrics =
         `I used to rule the world
-Seas would rise when I gave the word
-Now in the morning, I sleep alone
-Sweep the streets I used to own
-I used to roll the dice
-Feel the fear in my enemy's eyes`
+            Seas would rise when I gave the word
+            Now in the morning, I sleep alone
+            Sweep the streets I used to own
+            I used to roll the dice
+            Feel the fear in my enemy's eyes`
 
     return (
         <div className="song-details"
-            style={{ background: `linear-gradient(to bottom, ${bgColor} 0%, #121212 30%, #121212 100%)` }}        >
-            <SongDetailsHeader
-                song={songYT}
-                songDurationTxt={songDurationTxt}
-            />
-            <SongDetailsActions
-                song={songYT}
-            />
-            <section className="lyrics">
-                <h1>Lyrics</h1>
-                <p><LongTxt txt={lyrics} /></p>
-            </section>
-            <section className="artist-container list">
-                <div className="image-container">
-                    <img className="artist-img" src="https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png" alt="Artist img"></img>
+            style={{ background: `linear-gradient(to bottom, ${bgColor} 0%, #121212 30%, #121212 100%)` }}
+        >
+            {isLoading && <div>Loading...</div>}
+            {song && (
+                <div>
+                    <SongDetailsHeader
+                        song={song}
+                        songDurationTxt={songDurationTxt}
+                    />
+                    <SongDetailsActions
+                        song={song}
+                    />
+                    <section className="lyrics">
+                        <h1>Lyrics</h1>
+                        <p><LongTxt txt={lyrics} /></p>
+                    </section>
+                    <section className="artist-container list">
+                        <div className="image-container">
+                            <img
+                                className="artist-img"
+                                src="https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png"
+                                alt="Artist img">
+                            </img>
+                        </div>
+                        <div className="text">
+                            <div className="type"> Artist </div>
+                            <div className="name">{song.artist}</div>
+                        </div>
+                    </section>
+                    <AddSongs
+                        value={song.artist}
+                        style={"recommended"}
+                    />
                 </div>
-                <div className="text">
-                    <div className="type"> Artist </div>
-                    <div className="name">{songYT.artist}</div>
-                </div>
-            </section>
+            )}
             <Footer />
         </div>
     )
 }
+
+
 
