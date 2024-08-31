be00a9cb7521a60a26c1e9eae02691b7a24b1a39
omribarouch <omri.barouch@safebreach.com>
2024-08-31 22:48:58 +0300

    Fix station sore issues and adjust SongList and SongPreview

--- src/cmps/SongDetails/SongList.jsx
+++ src/cmps/SongDetails/SongList.jsx
@@ -1,21 +1,20 @@
 import { SongPreview } from '../SongDetails/SongPreview.jsx'
 import { SvgIcon } from '../SvgIcon.jsx'
 
-export function SongList({ station, songs, onRemoveSong }) {
+export function SongList({ station, onRemoveSong }) {
 
     return (
         <div className="song-list">
-            {(songs.length > 0) && <header className="list-style">
+            <div className="list-header dynamic-grid">
                 <div>#</div>
                 <div>Title</div>
                 <div>Album</div>
                 <div className="duration"><SvgIcon iconName={"duration"} /></div>
-
-            </header>}
-            <ul>
-                {songs.map((song, index) =>
+            </div>
+            <ul className="list-body">
+                {station.songs.map((song, index) =>
                     <li key={song.songId} >
-                        <SongPreview station={station} song={song} index={index + 1} style={'list-style'} onRemoveSong={onRemoveSong} />
+                        <SongPreview type={'list'} onRemoveSong={onRemoveSong} station={station} song={song} index={index + 1} />
                     </li>)
                 }
             </ul>
