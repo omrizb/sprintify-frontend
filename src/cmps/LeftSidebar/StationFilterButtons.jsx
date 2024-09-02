8e73bea451284cf91d388c26f51e840def5e4e4f
Darr Or <darrmorgan@gmail.com>
2024-09-02 11:54:44 -0400

    Refactor more station filter buttons

--- src/cmps/LeftSidebar/StationFilterButtons.jsx
+++ src/cmps/LeftSidebar/StationFilterButtons.jsx
@@ -1,55 +1,80 @@
+import { useState, useEffect } from 'react'
+
 import { updateFilterBy } from '../../store/actions/filterBy.actions'
 
 
 export function StationFilterButtons({ filterBy, userId }) {
 
     const sprintifyId = 'BBBB'
+    const [filterBtns, setFilterBtns] = useState([])
 
-
-    const filterBtns = [
+    const filterBtnsInit = [
         buildBtnObj({
             name: 'X',
+            type: '',
             showBtn: false,
-            onClick: () => updateFilterBy({ ...filterBy, type: '', createdBy: '' })
+            onClick: onClickBtn
         }),
         buildBtnObj({
             name: 'Playlists',
-            onClick: () => updateFilterBy({ ...filterBy, type: 'playlist', createdBy: '' })
+            onClick: onClickBtn
         }),
         buildBtnObj({
             name: 'Artists',
+            type: 'artist',
             showBtn: false,
             onClick: () => console.log('artist')
         }),
         buildBtnObj({
             name: 'Albums',
+            type: 'album',
             showBtn: false,
             onClick: () => updateFilterBy({ ...filterBy, type: 'album' })
         }),
         buildBtnObj({
             name: 'By you',
+            createdBy: userId,
             showBtn: true,
-            onClick: () => updateFilterBy({ ...filterBy, type: 'playlist', createdBy: userId })
+            onClick: onClickBtn
         }),
         buildBtnObj({
             name: 'By Sprintify',
+            createdBy: sprintifyId,
             showBtn: true,
-            onClick: () => updateFilterBy({ ...filterBy, type: 'playlist', createdBy: sprintifyId })
+            onClick: onClickBtn
         }),
     ]
+
+    useEffect(() => {
+        setFilterBtns(filterBtnsInit)
+    }, [])
 
 
     function buildBtnObj(props) {
         return {
             name: '',
+            type: 'playlist',
+            createdBy: '',
             showBtn: true,
-            btnDisp: '',
+            btnClass: '',
             onClick: noop,
             ...props
         }
     }
 
     function noop() { }
+
+    function onClickBtn(index, filterBtn) {
+        console.log(index)
+        console.log(filterBtn.type)
+        setFilterBtns(prevFilterBtns =>
+            prevFilterBtns.map((btn, i) =>
+                i === index ? { ...btn, btnClass: 'active' } : { ...btn, btnClass: '' }
+            )
+        )
+        updateFilterBy({ ...filterBy, type: filterBtn.type, createdBy: filterBtn.createdBy })
+
+    }
 
 
     return (
@@ -60,8 +85,8 @@ export function StationFilterButtons({ filterBy, userId }) {
                     {filterBtn.showBtn &&
 
                         <button
-                            className="btn-tinted clear-filter"
-                            onClick={filterBtn.onClick}>
+                            className={`btn-tinted clear-filter ${filterBtn.btnClass}`}
+                            onClick={() => filterBtn.onClick(index, filterBtn)}>
                             {filterBtn.name}
                         </button>}
                 </li>)
