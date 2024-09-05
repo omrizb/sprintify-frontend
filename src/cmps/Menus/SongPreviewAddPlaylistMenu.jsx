44df91e73b74a54d50cd632bf6653577ffd8bba4
Darr Or <darrmorgan@gmail.com>
2024-09-05 01:42:13 -0400

    Separated add a playlist from song preview to a separate component in menus

---
+++ src/cmps/Menus/SongPreviewAddPlaylistMenu.jsx
@@ -0,0 +1,134 @@
+import { useState, useEffect } from 'react'
+import { DropDownMenu } from './DropDownMenu'
+import { updateStation } from '../../store/actions/station.actions'
+
+export function SongPreviewAddPlaylistMenu({ setShowMenu, song, myStations, likedSongsStation }) {
+
+    const songId = song.songId
+    const [listItems, setListItems] = useState([])
+
+    useEffect(() => {
+        if (!myStations) return
+        setListItems(buildMyStationsArr())
+
+    }, [myStations])
+
+    function buildMyStationsArr() {
+        const titleObj = buildListObj({
+            type: 'title',
+            name: 'Add to playlist',
+            icon: ''
+        })
+        const newStationObj = buildListObj({
+            type: 'list-item',
+            name: 'New Playlist',
+            icon: 'plus',
+            onClick: handleAddStation
+        })
+        const stationsWithoutLiked = myStations.filter(station => station !== likedSongsStation)
+
+        const isSongLiked = likedSongsStation.songs.some(song => song.songId === songId)
+        const likedStationObj = buildListObj({
+            name: likedSongsStation.name,
+            station: likedSongsStation,
+            imgUrl: `https://misc.scdn.co/liked-songs/liked-songs-64.png`,
+            isChecked: isSongLiked
+
+        })
+        const stationList = stationsWithoutLiked.map(station => {
+            const isInStation = station.songs.some(song => song.songId === songId)
+            return buildListObj({
+                name: station.name,
+                station: station,
+                icon: 'musicSmall',
+                imgUrl: station.stationImgUrl,
+                isChecked: isInStation
+            })
+        })
+
+        const saveBtn = buildListObj({
+            name: 'Save',
+            type: 'button',
+            onClick: (list) => handleSave(list)
+        })
+
+        return [titleObj, newStationObj, likedStationObj, ...stationList, saveBtn]
+    }
+
+    function buildListObj(props) {
+        return {
+            type: 'checkBox',
+            name: '',
+            id: '',
+            icon: '',
+            imgUrl: '',
+
+            onClick: noop,
+            input: true,
+            isChecked: false,
+            inputFunction: (ev, listItem) => handleChecboxChange(ev, listItem),
+            ...props
+        }
+    }
+
+    function noop() { }
+
+
+    async function handleAddStation() {
+        setShowMenu(false)
+        try {
+            const station = await addStation()
+            const updatedStation = { ...station, songs: [song] }
+            const savedStation = await updateStation(updatedStation)
+            navigate(`/station/${savedStation._id}`)
+        } catch (err) {
+            console.log('Cannot add a station')
+        }
+    }
+
+    async function handleSave(list) {
+
+        setShowMenu(false)
+        const checkedItems = list?.filter(item => item.isChecked)
+        const unCheckedItems = list?.filter(item => (!item.isChecked && (item.type === 'checkBox')))
+
+        const checkedStations = checkedItems.map(item => item = item.station)
+        const unCheckedStations = unCheckedItems.map(item => item = item.station)
+
+        // console.log(song.songName)
+        // checkedStations.forEach(item => console.log('checked:', item.name, item._id))
+        // unCheckedStations.forEach(item => console.log('unChecked:', item.name, item._id))
+
+        const stationsToAdd = checkedStations.filter(station =>
+
+            !station.songs.some(song => song.songId === songId)
+        )
+
+        const updatedAdd = stationsToAdd.map(station => {
+            const updatedSongs = [...station.songs, song]
+            return { ...station, songs: updatedSongs }
+        })
+
+
+        const stationsToRemove = unCheckedStations.filter(station => {
+
+            return station.songs.some(song => song.songId === songId);
+        })
+
+        const updatedRemoved = stationsToRemove.map(station => {
+            const updatedSongs = station.songs.filter(song => song.songId != songId)
+            return { ...station, songs: updatedSongs }
+        })
+
+        const updatedStations = [...updatedAdd, ...updatedRemoved]
+
+        updatedStations.forEach(station => updateStation(station))
+
+    }
+
+    return (
+        <DropDownMenu listItems={listItems} />
+    )
+
+
+}
--- src/cmps/SongDetails/SongPreview.jsx
+++ src/cmps/SongDetails/SongPreview.jsx
@@ -1,6 +1,6 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import { useSelector } from 'react-redux'
-import { Link, useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 import { utilService } from '../../services/util.service.js'
 import { imgService } from '../../services/imgService.js'
@@ -8,14 +8,11 @@ import { AddToButton } from '../Buttons/AddToButton.jsx'
 import { VButton } from '../Buttons/VButton.jsx'
 import { DotsButton } from '../Buttons/DotsButton.jsx'
 import { PlayButton } from '../Buttons/PlayButton.jsx'
-import { updateLikedSongsStation, addStation, updateStation } from '../../store/actions/station.actions.js'
-import { DropDownMenu } from '../Menus/DropDownMenu.jsx'
+import { updateLikedSongsStation } from '../../store/actions/station.actions.js'
 import { SongPreviewActionsMenu } from '../Menus/SongPreviewActionsMenu.jsx'
+import { SongPreviewAddPlaylistMenu } from '../Menus/SongPreviewAddPlaylistMenu.jsx'
 
 export function SongPreview(props) {
-
-
-    const navigate = useNavigate()
 
     const playerSongId = useSelector(store => store.playerModule.player.song.songId)
     const isPlaying = useSelector(store => store.playerModule.player.isPlaying)
@@ -23,7 +20,7 @@ export function SongPreview(props) {
     const [showMoreMenu, setShowMoreMenu] = useState(false)
 
     const { song, stationId, likedSongsStation, myStations, hoveredSongId,
-        selectedSongId, index, type, onRemoveSong, isOwnedByUser, station } = props
+        selectedSongId, index, type, isOwnedByUser, station } = props
 
     let articleClassType
     switch (type) {
@@ -44,127 +41,6 @@ export function SongPreview(props) {
         isHighlighted ? 'highlight' : '',
         isCurrentlyPlayedSong ? 'currently-playing' : ''
     ].join(' ')
-
-    const [listItems, setListItems] = useState([])
-
-    useEffect(() => {
-        if (!myStations) return
-        setListItems(buildMyStationsArr())
-
-    }, [myStations])
-
-    function buildMyStationsArr() {
-        const titleObj = buildListObj({
-            type: 'title',
-            name: 'Add to playlist',
-            icon: ''
-        })
-        const newStationObj = buildListObj({
-            type: 'list-item',
-            name: 'New Playlist',
-            icon: 'plus',
-            onClick: handleAddStation
-        })
-        const stationsWithoutLiked = myStations.filter(station => station !== likedSongsStation)
-
-        const isSongLiked = likedSongsStation.songs.some(song => song.songId === songId)
-        const likedStationObj = buildListObj({
-            name: likedSongsStation.name,
-            station: likedSongsStation,
-            imgUrl: `https://misc.scdn.co/liked-songs/liked-songs-64.png`,
-            isChecked: isSongLiked
-
-        })
-        const stationList = stationsWithoutLiked.map(station => {
-            const isInStation = station.songs.some(song => song.songId === songId)
-            return buildListObj({
-                name: station.name,
-                station: station,
-                icon: 'musicSmall',
-                imgUrl: station.stationImgUrl,
-                isChecked: isInStation
-            })
-        })
-
-        const saveBtn = buildListObj({
-            name: 'Save',
-            type: 'button',
-            onClick: (list) => handleSave(list)
-        })
-
-        return [titleObj, newStationObj, likedStationObj, ...stationList, saveBtn]
-    }
-
-    function buildListObj(props) {
-        return {
-            type: 'checkBox',
-            name: '',
-            id: '',
-            icon: '',
-            imgUrl: '',
-
-            onClick: noop,
-            input: true,
-            isChecked: false,
-            inputFunction: (ev, listItem) => handleChecboxChange(ev, listItem),
-            ...props
-        }
-    }
-
-    function noop() { }
-
-
-    async function handleAddStation() {
-        setShowMenu(false)
-        try {
-            const station = await addStation()
-            const updatedStation = { ...station, songs: [song] }
-            const savedStation = await updateStation(updatedStation)
-            navigate(`/station/${savedStation._id}`)
-        } catch (err) {
-            console.log('Cannot add a station')
-        }
-    }
-
-    async function handleSave(list) {
-
-        setShowMenu(false)
-        const checkedItems = list?.filter(item => item.isChecked)
-        const unCheckedItems = list?.filter(item => (!item.isChecked && (item.type === 'checkBox')))
-
-        const checkedStations = checkedItems.map(item => item = item.station)
-        const unCheckedStations = unCheckedItems.map(item => item = item.station)
-
-        // console.log(song.songName)
-        // checkedStations.forEach(item => console.log('checked:', item.name, item._id))
-        // unCheckedStations.forEach(item => console.log('unChecked:', item.name, item._id))
-
-        const stationsToAdd = checkedStations.filter(station =>
-
-            !station.songs.some(song => song.songId === songId)
-        )
-
-        const updatedAdd = stationsToAdd.map(station => {
-            const updatedSongs = [...station.songs, song]
-            return { ...station, songs: updatedSongs }
-        })
-
-
-        const stationsToRemove = unCheckedStations.filter(station => {
-
-            return station.songs.some(song => song.songId === songId);
-        })
-
-        const updatedRemoved = stationsToRemove.map(station => {
-            const updatedSongs = station.songs.filter(song => song.songId != songId)
-            return { ...station, songs: updatedSongs }
-        })
-
-        const updatedStations = [...updatedAdd, ...updatedRemoved]
-
-        updatedStations.forEach(station => updateStation(station))
-
-    }
 
 
     function addSong() {
@@ -209,7 +85,12 @@ export function SongPreview(props) {
 
                 <div className="add-btn-container" onClick={addSong}>
                     {<DynamicButton isHighlighted={isHighlighted} isLikedByUser={isLikedByUser} />}
-                    {showMenu && <DropDownMenu listItems={listItems} />}
+                    {/* {showMenu && <DropDownMenu listItems={listItems} />} */}
+                    {showMenu && <SongPreviewAddPlaylistMenu
+                        song={song}
+                        myStations={myStations}
+                        likedSongsStation={likedSongsStation}
+                        setShowMenu={setShowMenu} />}
                 </div>
                 <span>{utilService.getTimeStr(duration)}</span>
                 <div onClick={() => setShowMoreMenu(prevShowMoreMenu => !prevShowMoreMenu)}
