b4ccb49b7d0876e6d37d5c84bb287e4346d04f2d
omribarouch <omri.barouch@safebreach.com>
2024-09-07 02:14:01 +0300

    Adjust MainView

--- src/pages/MainView.jsx
+++ src/pages/MainView.jsx
@@ -1,16 +1,16 @@
-import { array } from "prop-types"
-import { MainViewHeader } from "../cmps/MainView/MainViewHeader"
-import { MainViewBody } from "../cmps/MainView/MainViewBody"
+import { useState } from 'react'
+import { MainViewHeader } from '../cmps/MainView/MainViewHeader'
+import { MainViewBody } from '../cmps/MainView/MainViewBody'
 
 export function MainView() {
 
-    
+    const [bgColor, setBgColor] = useState(null)
 
     return (
         <div className="main-view">
+            <div className="secondary-background" style={{ backgroundColor: 'gray' }}></div>
             <MainViewHeader />
             <MainViewBody />
-        
         </div>
     )
 }
