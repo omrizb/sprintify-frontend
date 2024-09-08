b8e53f0850128cb1a0a06d9a281279c312e879bb
Darr Or <darrmorgan@gmail.com>
2024-09-07 22:15:40 -0400

    Add header fixer to song details page

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -4,6 +4,9 @@ import { useParams } from 'react-router-dom'
 import { spotifyService } from '../services/spotify.service.js'
 import { DetailsPageHeader } from '../cmps/Headers/DetailsPageHeader.jsx'
 import { Loader } from '../cmps/Loader.jsx'
+import { HeaderFixer } from '../cmps/HeaderFixer.jsx'
+import { PlayButton } from '../cmps/Buttons/PlayButton.jsx'
+import { colorUtilService } from '../services/color.util.service.js'
 
 
 export function SongDetails() {
@@ -11,6 +14,7 @@ export function SongDetails() {
     const { id } = useParams()
     const [isLoading, setIsLoading] = useState(true)
     const [song, setSong] = useState({})
+    const [bgColor, setBgColor] = useState()
 
 
 
@@ -34,11 +38,37 @@ export function SongDetails() {
     return (isLoading)
         ? <Loader /> :
         <div className="song-details">
+            <HeaderFixer
+                header={renderHeader(song)}
+                className="padded-top-rounded-box"
+                bgColor={bgColor && colorUtilService.adjustBrightness(bgColor, 0.4)}
+                showFromY={150}
+            >
 
-            <DetailsPageHeader song={song} pageType={'song'} />
+                <DetailsPageHeader
+                    song={song}
+                    pageType={'song'}
+                    onSetBgColor={setBgColor} />
+            </HeaderFixer>
 
         </div>
 
+}
+
+function renderHeader(song) {
+    return (
+        <div className="flex-regular-gap">
+            {song &&
+                <>
+                    <PlayButton
+                        type="song-preview"
+                        song={song}
+                    />
+                    <div className="title-medium">{song.name}</div>
+                </>
+            }
+        </div>
+    )
 }
 
 
