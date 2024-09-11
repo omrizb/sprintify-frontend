68cc2876e7f81ce69b43b06df7493a6a01e5a8bd
Darr Or <darrmorgan@gmail.com>
2024-09-11 00:49:02 -0400

    Move the creating made for you collection to the service

--- src/cmps/MainView/MainViewBody.jsx
+++ src/cmps/MainView/MainViewBody.jsx
@@ -3,11 +3,10 @@ import { useSelector } from 'react-redux'
 
 import { utilService } from '../../services/util.service'
 
-import { stationService } from '../../services/station/station.service.remote'
 import { Footer } from '../Footer'
 import { StationSection } from '../StationSection'
 import { StationListMainView } from './StationListMainView'
-import { addSprintifyStation, addStation } from '../../store/actions/station.actions'
+import madeForYouCollections from '../../services/station/station.service.remote'
 
 
 export function MainViewBody({ onSetBgColor }) {
@@ -34,47 +33,10 @@ export function MainViewBody({ onSetBgColor }) {
         const mainStations = utilService.getRandomItems(myLibrary, numOfStations)
 
         setStationsMain(mainStations)
-
-        const stationsForYou = createMadeForYou()
-        // setMadeForYou(stationsForYou)
+        setMadeForYou(madeForYouCollections)
     }
 
 
-
-    async function createMadeForYou() {
-        const stationCovers = [
-            'https://dailymix-images.scdn.co/v2/img/ab6761610000e5eb7c774b7b4da216c33782c193/1/en/default',
-            'https://dailymix-images.scdn.co/v2/img/ab6761610000e5ebee0bf92f5269431b705722b2/2/en/default',
-            'https://dailymix-images.scdn.co/v2/img/ab6761610000e5eb7bff9168ecfa90aca537adee/3/en/default',
-            'https://dailymix-images.scdn.co/v2/img/ab6761610000e5ebb59c9bdf20c6eb811f9aa894/4/en/default',
-            'https://dailymix-images.scdn.co/v2/img/ab6761610000e5ebed7d082db2925fe99a9b9487/5/en/default',
-            'https://dailymix-images.scdn.co/v2/img/ab6761610000e5eb92883b0e094a36d2f43ad284/6/en/default',
-        ]
-        const stationsForYou = []
-        const savedStations = []
-
-        for (let i = 0; i < 5; i++) {
-            stationsForYou[i] = stationService.getEmptyStation()
-            stationsForYou[i].name = `Daily Mix ${i + 1}`
-            stationsForYou[i].createdBy = { id: 'BBBB', fullName: 'Sprintify', imgUrl: '' }
-            stationsForYou[i].stationImgUrl = stationCovers[i]
-            stationsForYou[i].isOwnedByUser = false
-
-            const songsArrs = stations.map(station => {
-
-                const numOfSongs = (station.songs.length < 3) ? station.songs.length : 3
-                const songs = utilService.getRandomItems(station.songs, numOfSongs)
-                return songs
-            })
-            stationsForYou[i].songs = songsArrs.flat()
-            savedStations[i] = await stationService.save(stationsForYou[i])
-        }
-
-        console.log(savedStations)
-
-        setMadeForYou(savedStations)
-        return savedStations
-    }
 
     return (
         <div className="main-view-body">
--- src/services/station/station.service.remote.js
+++ src/services/station/station.service.remote.js
@@ -1,6 +1,9 @@
 import { httpService } from '../http.service'
 import { utilService } from '../util.service'
 
+const madeForYouCollections = await getMadeForYou()
+export default madeForYouCollections
+
 export const stationService = {
     getEmptyStation,
     query,
@@ -9,7 +12,7 @@ export const stationService = {
     remove,
     getRecentlyPlayed,
     getTopMixes,
-    getMadeForYou,
+    // getMadeForYou,
     getLikedSongsStation
 }
 
@@ -110,9 +113,47 @@ async function getTopMixes(userId, size = 4) {
     //TODO write algorithm for fetching top mixes per user 
     return httpService.get(`station`, { stationType: 'playlist' })
 }
-async function getMadeForYou(stations) {
 
 
+async function getMadeForYou() {
 
+    const stations = await query({})
+
+    const collections = stations.filter(station => station.type === 'daily-mix')
+    if (collections.length > 0) return collections
+
+    const stationCovers = [
+        'https://dailymix-images.scdn.co/v2/img/ab6761610000e5eb7c774b7b4da216c33782c193/1/en/default',
+        'https://dailymix-images.scdn.co/v2/img/ab6761610000e5ebee0bf92f5269431b705722b2/2/en/default',
+        'https://dailymix-images.scdn.co/v2/img/ab6761610000e5eb7bff9168ecfa90aca537adee/3/en/default',
+        'https://dailymix-images.scdn.co/v2/img/ab6761610000e5ebb59c9bdf20c6eb811f9aa894/4/en/default',
+        'https://dailymix-images.scdn.co/v2/img/ab6761610000e5ebed7d082db2925fe99a9b9487/5/en/default',
+        'https://dailymix-images.scdn.co/v2/img/ab6761610000e5eb92883b0e094a36d2f43ad284/6/en/default',
+    ]
+    const stationsForYou = []
+    const savedStations = []
+
+    for (let i = 0; i < 5; i++) {
+        stationsForYou[i] = getEmptyStation()
+        stationsForYou[i].name = `Daily Mix ${i + 1}`
+        stationsForYou[i].createdBy = { id: 'BBBB', fullName: 'Sprintify', imgUrl: '' }
+        stationsForYou[i].stationImgUrl = stationCovers[i]
+        stationsForYou[i].isOwnedByUser = false
+        stationsForYou[i].type = 'daily-mix'
+
+        const songsArrs = stations.map(station => {
+
+            const numOfSongs = (station.songs.length < 3) ? station.songs.length : 3
+            const songs = utilService.getRandomItems(station.songs, numOfSongs)
+            return songs
+        })
+        stationsForYou[i].songs = songsArrs.flat()
+
+        savedStations[i] = await save(stationsForYou[i])
+    }
+
+    return savedStations
 }
+
+
 
